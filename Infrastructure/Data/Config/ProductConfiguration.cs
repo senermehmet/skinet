cfec0599ba466a4dc1f08cd7c112e0519074a000
senermehmet <mehmet.kim@gmail.com>
2022-01-04 21:14:59 +0300

    First Commit ev

--- Infrastructure/Data/Config/ProductConfiguration.cs
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -10,8 +10,8 @@ namespace Infrastructure.Data.Config
         {
             builder.Property(p => p.Id).IsRequired();
             builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
-            builder.Property(p => p.Description).IsRequired().HasMaxLength(180);
-            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+            builder.Property(p => p.Description).IsRequired();
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)").HasDefaultValue(0);
             builder.Property(p => p.PictureUrl).IsRequired();
             builder.HasOne(b => b.ProductBrand).WithMany().HasForeignKey(p => p.ProductBrandId);
             builder.HasOne(t => t.ProductType).WithMany().HasForeignKey(p => p.ProductTypeID);
@@ -21,14 +21,14 @@ namespace Infrastructure.Data.Config
     {
         public void Configure(EntityTypeBuilder<ProductBrand> builder)
         {
-            builder.Property(b =>b.Name).IsRequired().HasMaxLength(100);
+            builder.Property(b =>b.Name).IsRequired().HasMaxLength(250);
         }
     }
  public  class ProductTypeConfiguration : IEntityTypeConfiguration<ProductType>
     {
         public void Configure(EntityTypeBuilder<ProductType> builder)
         {
-            builder.Property(t =>t.Name).IsRequired().HasMaxLength(100);
+            builder.Property(t =>t.Name).IsRequired().HasMaxLength(250);
         }
     }
 
--- Infrastructure/Data/StoreContextSeedData.cs
+++ Infrastructure/Data/StoreContextSeedData.cs
@@ -8,39 +8,38 @@ namespace Infrastructure.Data
     {
         public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
         {
+
             try
             {
                 if (!context.ProductBrands.Any())
                 {
                     var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
                     var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
-                    foreach (var brandItem in brands)
+                    foreach (var item in brands)
                     {
-                        context.ProductBrands.Add(brandItem);
+                        context.ProductBrands.Add(item);
                     }
                     await context.SaveChangesAsync();
                 }
-
                 if (!context.ProductTypes.Any())
                 {
                     var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
                     var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
-                    foreach (var typeItem in types)
+                    foreach (var item in types)
                     {
-                        context.ProductTypes.Add(typeItem);
+                        context.ProductTypes.Add(item);
                     }
-
                     await context.SaveChangesAsync();
                 }
+
                 if (!context.Products.Any())
                 {
-                    var productsData = File.ReadAllText("../Infrastructure/SeedData/products.json");
+                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
                     var products = JsonSerializer.Deserialize<List<Product>>(productsData);
-                    foreach (var productItem in products)
+                    foreach (var item in products)
                     {
-                        context.Products.Add(productItem);
+                        context.Products.Add(item);
                     }
-
                     await context.SaveChangesAsync();
                 }
             }
